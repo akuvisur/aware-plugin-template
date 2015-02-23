193955f91dbc10c5e86a583b61eaf7cf338936fb
Teemu Partanen <teemupart@gmail.com>
2015-02-23 18:35:57 +0200

    Test card

--- app/src/main/java/com/aware/plugin/template/ContextCard.java
+++ app/src/main/java/com/aware/plugin/template/ContextCard.java
@@ -3,22 +3,62 @@ package com.aware.plugin.template;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.util.Log;
+import com.aware.Magnetometer;
 
+import com.aware.Aware;
+import com.aware.Aware_Preferences;
+import com.aware.utils.Aware_Plugin;
 import com.aware.utils.IContextCard;
 
-public class ContextCard implements IContextCard {
+public class ContextCard implements IContextCard, View.OnClickListener {
 
     //Empty constructor used to instantiate this card
+    private final String INSIDE = "IN";
+    private final String OUTSIDE = "OUT";
+    private Context context;
+
+    private String label = null;
+
     public ContextCard(){};
 
     @Override
     public View getContextCard(Context context) {
+
+
         //Inflate and return your card's layout. See LayoutInflater documentation.
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View card = (View) inflater.inflate(R.layout.card, null);
 
+        this.context = context;
+
         //Modify card's content
 
+        Button in = (Button)card.findViewById(R.id.inbutton);
+        in.setOnClickListener(this);
+        Button out = (Button)card.findViewById(R.id.outbutton2);
+        out.setOnClickListener(this);
+
         return card;
+
     }
+
+    @Override
+    public void onClick(View view){
+        switch(view.getId()){
+            case R.id.inbutton:
+                /*Intent newLabel = new Intent(Magnetometer.ACTION_AWARE_MAGNETOMETER_LABEL);
+                newLabel.putExtra(Magnetometer.EXTRA_LABEL, INSIDE);
+                context.sendBroadcast(newLabel);*/
+                break;
+            case R.id.outbutton2:
+                break;
+            default:
+                break;
+        }
+
+    }
+
 }
--- app/src/main/java/com/aware/plugin/template/Plugin.java
+++ app/src/main/java/com/aware/plugin/template/Plugin.java
@@ -1,12 +1,19 @@
 package com.aware.plugin.template;
 
+import android.content.Intent;
+import android.hardware.SensorManager;
+import android.net.Uri;
 import android.util.Log;
+import android.view.View;
+import com.aware.Magnetometer;
 
 import com.aware.Aware;
 import com.aware.Aware_Preferences;
 import com.aware.utils.Aware_Plugin;
 
 public class Plugin extends Aware_Plugin {
+
+    static final String TEST = "asd";
 
     @Override
     public void onCreate() {
@@ -15,13 +22,37 @@ public class Plugin extends Aware_Plugin {
         TAG = "Template";
         DEBUG = Aware.getSetting(this, Aware_Preferences.DEBUG_FLAG).equals("true");
 
-        if( DEBUG ) Log.d(TAG, "Template plugin running");
+        if (DEBUG) Log.d(TAG, "Template plugin running");
+
+        Aware.setSetting(getApplicationContext(), Aware_Preferences.STATUS_MAGNETOMETER, true);
+        //Aware.setSetting(getApplicationContext(), Aware_Preferences.FREQUENCY_MAGNETOMETER, SensorManager.SENSOR_DELAY_NORMAL);
+        /*Aware.setSetting(getApplicationContext(), Aware_Preferences.STATUS_TELEPHONY, true);
+
+        Aware.setSetting(getApplicationContext(), Aware_Preferences.STATUS_LOCATION_GPS, true);*/
+
+
+        Intent newLabel = new Intent(Magnetometer.ACTION_AWARE_MAGNETOMETER_LABEL);
+        newLabel.putExtra(Magnetometer.EXTRA_LABEL, "inside");
+        sendBroadcast(newLabel);
+
+        Intent applySettings = new Intent(Aware.ACTION_AWARE_REFRESH);
+        sendBroadcast(applySettings);
+
     }
+
 
     @Override
     public void onDestroy() {
         super.onDestroy();
 
-        if( DEBUG ) Log.d(TAG, "Template plugin terminated");
+        if (DEBUG) Log.d(TAG, "Template plugin terminated");
+        Aware.setSetting(getApplicationContext(), Aware_Preferences.STATUS_MAGNETOMETER, false);
+        Aware.setSetting(getApplicationContext(), Aware_Preferences.STATUS_TELEPHONY, false);
+        Aware.setSetting(getApplicationContext(), Aware_Preferences.STATUS_LOCATION_GPS, false);
+        Intent applySettings = new Intent(Aware.ACTION_AWARE_REFRESH);
+        sendBroadcast(applySettings);
+        //Aware.setSetting(getApplicationContext(), Aware_Preferences.STATUS_MAGNETOMETER, false);
+        //Aware.setSetting(getApplicationContext(), Aware_Preferences.STATUS_TELEPHONY, false);
     }
+
 }
